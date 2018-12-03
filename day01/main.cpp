6b1e1a96e8cddf456159c214d9d69119dbc20a94
Petr Faltýnek <pfaltynek@gmail.com>
2018-12-03 10:15:18 +0100

    day 1 part 2 finished

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-//#include <string>
+#include <unordered_set>
 
 int main(void) {
 	int result1 = 0, result2 = 0;
@@ -9,6 +9,9 @@ int main(void) {
 	std::string line;
 	std::string captcha;
 	std::vector<int32_t> freq_diffs;
+	std::unordered_set<int32_t> result_freq;
+	bool result2_found = false;
+	unsigned int i;
 
 	std::cout << "=== Advent of Code 2018 - day 1 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
@@ -21,6 +24,8 @@ int main(void) {
 	}
 
 	freq_diffs.clear();
+	result_freq.clear();
+
 	while (std::getline(input, line)) {
 		freq_diffs.push_back(std::stol(line, nullptr));
 	}
@@ -29,8 +34,20 @@ int main(void) {
 		input.close();
 	}
 
-	for (unsigned int i = 0; i < freq_diffs.size(); ++i){
+	for (i = 0; i < freq_diffs.size(); ++i) {
 		result1 += freq_diffs[i];
+	}
+
+	i = 0;
+	while (!result2_found) {
+		result2 += freq_diffs[i];
+		if (result_freq.count(result2)) {
+			result2_found = true;
+			break;
+		} else {
+			result_freq.insert(result2);
+		}
+		i = (++i % freq_diffs.size());
 	}
 
 	std::cout << "Result is " << result1 << std::endl;
