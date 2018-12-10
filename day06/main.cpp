5c4c0e10478e97eaf1a28ffc89a82967a913c203
Petr Faltýnek <pfaltynek@gmail.com>
2018-12-10 11:40:36 +0100

    day 6 part1 finished

--- day06/main.cpp
+++ day06/main.cpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
-#define TEST1 1
+#define TEST1 0
 #define TEST2 0
 
 typedef struct limits {
@@ -16,6 +16,7 @@ typedef struct limits {
 } limits_t;
 
 const std::regex coord_template("(\\d+), (\\d+)");
+const int extra = 10;
 
 limits_t GetCoordLimits(const std::vector<std::pair<int32_t, int32_t>> &coords) {
 	limits_t result{};
@@ -40,17 +41,71 @@ limits_t GetCoordLimits(const std::vector<std::pair<int32_t, int32_t>> &coords) 
 		}
 	}
 
-	result.x_min--;
-	result.x_max++;
-	result.y_min--;
-	result.y_max++;
-
 	return result;
 }
 
 void FillClosestCoordinateMap(const std::vector<std::pair<int32_t, int32_t>> coords, const limits_t limits,
 							  std::unordered_map<std::string, std::pair<int32_t, int32_t>> &map) {
-	
+	uint32_t i, j, k;
+
+	for (i = 0; i < coords.size(); ++i) {
+		for (j = limits.x_min - extra; j <= limits.x_max + extra; ++j) {
+			for (k = limits.y_min - extra; k <= limits.y_max + extra; ++k) {
+				std::string key = std::to_string(j) + "x" + std::to_string(k);
+				int32_t distance = abs(coords[i].first - j) + abs(coords[i].second - k);
+				if (!i) {
+					map[key] = std::pair<int32_t, int32_t>(i + 1, distance);
+				} else {
+					if (map[key].second > distance) {
+						map[key].first = i + 1;
+						map[key].second = distance;
+					} else if (map[key].second == distance) {
+						map[key].first = 0;
+					}
+				}
+			}
+		}
+	}
+
+	/*	for (i = 0; i < coords.size(); ++i) {
+			map.erase(std::to_string(coords[i].first) + "x" + std::to_string(coords[i].second));
+		}*/
+}
+
+int32_t GetLargestFiniteAreaSize(const std::vector<std::pair<int32_t, int32_t>> coords, const limits_t limits,
+								 std::unordered_map<std::string, std::pair<int32_t, int32_t>> map) {
+	uint32_t i, j, k;
+	int32_t result = 0;
+	std::unordered_map<int32_t, std::pair<int32_t, int32_t>> sizes;
+
+	sizes.clear();
+
+	for (k = limits.y_min - extra; k <= limits.y_max + extra; ++k) {
+		for (j = limits.x_min - extra; j <= limits.x_max + extra; ++j) {
+			std::string key = std::to_string(j) + "x" + std::to_string(k);
+			if (map[key].first) {
+				sizes[map[key].first].first++;
+			}
+		}
+	}
+
+	for (k = limits.y_min - (extra / 2); k <= limits.y_max + (extra / 2); ++k) {
+		for (j = limits.x_min - (extra / 2); j <= limits.x_max + (extra / 2); ++j) {
+			std::string key = std::to_string(j) + "x" + std::to_string(k);
+			if (map[key].first) {
+				sizes[map[key].first].second++;
+			}
+		}
+	}
+
+	for (auto it = sizes.begin(); it != sizes.end(); ++it) {
+		if (it->second.first == it->second.second) {
+			if (it->second.first > result) {
+				result = it->second.first;
+			}
+		}
+	}
+	return result;
 }
 
 bool DecodeCoord(const std::string line, std::vector<std::pair<int32_t, int32_t>> &coords) {
@@ -110,6 +165,10 @@ int main(void) {
 #endif
 
 	limits = GetCoordLimits(coords);
+
+	FillClosestCoordinateMap(coords, limits, map);
+
+	result1 = GetLargestFiniteAreaSize(coords, limits, map);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
