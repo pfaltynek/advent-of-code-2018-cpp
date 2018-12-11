b52f327b4401b39189f95011a8142b3c3100efd2
Petr Faltýnek <pfaltynek@gmail.com>
2018-12-11 14:44:38 +0100

    day 7 minor update

--- day07/main.cpp
+++ day07/main.cpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
-#define TEST1 0
+#define TEST1 1
 #define TEST2 0
 
 typedef struct limits {
@@ -17,14 +17,16 @@ typedef struct limits {
 
 const std::regex condition_template("^Step ([A-Z]) must be finished before step ([A-Z]) can begin.$");
 
-bool DecodeInstruction(const std::string line, std::vector<std::pair<char, char>> &conditions) {
+bool DecodeInstruction(const std::string line, std::unordered_map<char, std::string> &rules, std::map<char, int32_t> &counts) {
 	std::smatch sm;
 	char x, y;
 
 	if (std::regex_match(line, sm, condition_template)) {
 		x = sm.str(1)[0];
 		y = sm.str(2)[0];
-		conditions.push_back(std::pair<char, char>(x, y));
+		counts[x]++;
+		counts[y]++;
+		rules[x] += y;
 		return true;
 	}
 	return false;
@@ -34,14 +36,23 @@ int main(void) {
 	int result1 = 0, result2 = 0, cnt = 0;
 	std::ifstream input;
 	std::string line;
-	std::vector<std::pair<char, char>> conditions;
+	std::unordered_map<char, std::string> rules;
+	std::map<char, int32_t> counts;
 
 	std::cout << "=== Advent of Code 2018 - day 7 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
-	conditions.clear();
+	rules.clear();
+	counts.clear();
 
 #if TEST1
+	DecodeInstruction("Step C must be finished before step A can begin.", rules, counts);
+	DecodeInstruction("Step C must be finished before step F can begin.", rules, counts);
+	DecodeInstruction("Step A must be finished before step B can begin.", rules, counts);
+	DecodeInstruction("Step A must be finished before step D can begin.", rules, counts);
+	DecodeInstruction("Step B must be finished before step E can begin.", rules, counts);
+	DecodeInstruction("Step D must be finished before step E can begin.", rules, counts);
+	DecodeInstruction("Step F must be finished before step E can begin.", rules, counts);
 
 #elif TEST2
 
@@ -55,7 +66,7 @@ int main(void) {
 
 	while (std::getline(input, line)) {
 		cnt++;
-		if (!DecodeInstruction(line, conditions)) {
+		if (!DecodeInstruction(line, rules, counts)) {
 			std::cout << "Invalid instruction at line " << cnt << std::endl;
 		}
 	}
@@ -64,7 +75,6 @@ int main(void) {
 		input.close();
 	}
 #endif
-
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
