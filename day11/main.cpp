cebbc4d6a6a703733c975b8db40084b28791d5bd
Petr Faltýnek <pfaltynek@gmail.com>
2018-12-17 10:58:17 +0100

    daay 11 part 1 finished

--- day11/main.cpp
+++ day11/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <unordered_map>
 
-#define TEST1 1
+#define TEST1 0
 #define TEST2 0
 
 int32_t GetPowerLevel(uint32_t x, uint32_t y, uint32_t grid_sn) {
@@ -17,6 +17,38 @@ int32_t GetPowerLevel(uint32_t x, uint32_t y, uint32_t grid_sn) {
 	return result;
 }
 
+void GetLargestTotalPower(const int32_t grid_sn, int32_t grid_x, int32_t grid_y, int32_t area_x, int32_t area_y, uint32_t &result_x, uint32_t &result_y) {
+	int32_t grid[grid_x][grid_y];
+	int32_t max = INT32_MIN, sum;
+	uint32_t i, j, i1, j1;
+
+	result_x = 0;
+	result_y = 0;
+
+	for (i = 0; i < grid_x; ++i) {
+		for (j = 0; j < grid_y; ++j) {
+			grid[i][j] = GetPowerLevel(i + 1, j + 1, grid_sn);
+		}
+	}
+
+	for (i = 0; i < (grid_x - 3); ++i) {
+		for (j = 0; j < (grid_y - 3); ++j) {
+			sum = 0;
+			for (i1 = i; i1 < (i + 3); ++i1) {
+				for (j1 = j; j1 < (j + 3); ++j1) {
+					sum += grid[i1][j1];
+				}
+			}
+
+			if (sum > max) {
+				max = sum;
+				result_x = i + 1;
+				result_y = j + 1;
+			}
+		}
+	}
+}
+
 int main(void) {
 	int cnt = 0;
 	uint32_t result1x = 0, result1y = 0, result2 = 0, grid_sn;
@@ -29,10 +61,14 @@ int main(void) {
 #if TEST1
 	int test;
 
-	test = GetPowerLevel(3, 5, 8); // 4
-	test = GetPowerLevel(122, 79, 57); // -5
+	test = GetPowerLevel(3, 5, 8);		// 4
+	test = GetPowerLevel(122, 79, 57);  // -5
 	test = GetPowerLevel(217, 196, 39); // 0
 	test = GetPowerLevel(101, 153, 71); // 4
+
+	GetLargestTotalPower(18, 300, 300, 3, 3, result1x, result1y);
+	GetLargestTotalPower(42, 300, 300, 3, 3, result1x, result1y);
+
 #elif TEST2
 
 #else
@@ -45,10 +81,7 @@ int main(void) {
 
 	while (std::getline(input, line)) {
 		cnt++;
-		if (!DecodeElvPoint(line, points)) {
-			std::cout << "Invalid point data at line " << cnt << std::endl;
-			return -1;
-		}
+		grid_sn = stoi(line);
 	}
 
 	if (input.is_open()) {
@@ -56,6 +89,7 @@ int main(void) {
 	}
 #endif
 
+	GetLargestTotalPower(grid_sn, 300, 300, 3, 3, result1x, result1y);
 	std::cout << "Result is " << result1x << "," << result1y << std::endl;
 
 	std::cout << "--- part 2 ---" << std::endl;
