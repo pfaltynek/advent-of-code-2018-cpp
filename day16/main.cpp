3728a622aa5d989b5bf9133596763e3b2082d2c2
Petr Faltýnek <pfaltynek@gmail.com>
2019-10-01 21:43:49 +0200

    day16 - started

---
+++ day16/main.cpp
@@ -0,0 +1,17 @@
+#include "main.hpp"
+
+int main(void) {
+	uint64_t result1 = 0, result2 = 0;
+
+	std::cout << "=== Advent of Code 2018 - day 16 ====" << std::endl;
+	std::cout << "--- part 1 ---" << std::endl;
+
+	result1 = 1;
+
+	std::cout << "Result is " << result1 << std::endl;
+	std::cout << "--- part 2 ---" << std::endl;
+
+	result2 = 2;
+
+	std::cout << "Result is " << result2 << std::endl;
+}
