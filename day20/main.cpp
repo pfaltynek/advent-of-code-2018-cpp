271c12950ebc4c19864f6314d9e21adb818632a7
Petr Faltýnek <pfaltynek@gmail.com>
2019-10-15 10:00:28 +0200

    day20 - part1 ready for tests

--- day20/main.cpp
+++ day20/main.cpp
@@ -1,23 +1,24 @@
 #include "main.hpp"
 
-typedef struct STEP_INFO{
+typedef struct STEP_INFO {
 	coord_str coords;
 	int32_t steps;
 	int32_t doors;
 } step_info_str;
 
 class RoomsMap {
-	public:
-	  bool init(std::string& regex);
-	  void print();
-	  void get_furthest_room_passing_doors_count();
-	  bool decode_map(const std::string regex);
-	  char get_map_symbol(const coord_str coord);
+  public:
+	bool init(std::string& regex);
+	void print();
+	int32_t get_furthest_room_passing_doors_count();
+	bool decode_map(const std::string regex);
+	char get_map_symbol(const coord_str coord);
+	char get_map_symbol(int32_t x, int32_t y);
 
-	private:
-	  std::map<coord_str, char> map_;
-	  coord_str min_;
-	  coord_str max_;
+  private:
+	std::map<coord_str, char> map_;
+	coord_str min_;
+	coord_str max_;
 };
 
 bool RoomsMap::init(std::string& regex) {
@@ -139,12 +140,16 @@ bool RoomsMap::decode_map(const std::string regex) {
 	return true;
 }
 
-char RoomsMap::get_map_symbol(const coord_str coord){
+char RoomsMap::get_map_symbol(const coord_str coord) {
 	if (map_.count(coord)) {
 		return map_[coord];
 	} else {
 		return '#';
 	}
+}
+
+char RoomsMap::get_map_symbol(int32_t x, int32_t y) {
+	return get_map_symbol(coord_str(x, y));
 }
 
 void RoomsMap::print() {
@@ -162,35 +167,86 @@ void RoomsMap::print() {
 	std::cout << std::endl;
 }
 
-
-void RoomsMap::get_furthest_room_passing_doors_count(){
-	coord_str pt = {};
+int32_t RoomsMap::get_furthest_room_passing_doors_count() {
+	coord_str pt = {}, room;
 	step_info_str curr, tmp;
 	std::queue<step_info_str> queue;
 	std::map<coord_str, bool> history;
+	std::queue<coord_str> rooms;
+	std::map<coord_str, std::vector<step_info_str>> results;
+	std::vector<coord_str> nexts = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
 	char c;
+	int32_t result = 0;
 
-	history.clear();
-	while (!queue.empty()){
-		queue.pop();
-	}
+	rooms = {};
+	results.clear();
 
-	curr.coords = pt;
-	curr.doors = 0;
-	curr.steps = 0;
-	queue.emplace(curr);
-	history[pt] = true;
+	for (int32_t i = min_.x; i <= max_.x; i++) {
+		for (int32_t j = min_.y; j <= max_.y; j++) {
+			coord_str coord(i, j);
 
-	while (!queue.empty()){
-		curr = queue.front();
-		queue.pop();
-
-		if (!map_.count(curr.coords)) {
-			continue;
+			if (get_map_symbol(coord) == '.') {
+				rooms.emplace(coord);
+			}
 		}
-		c = map_[curr.coords];
-
 	}
+
+	while (!rooms.empty()) {
+		room = rooms.front();
+		rooms.pop();
+		history.clear();
+		queue = {};
+
+		curr.coords = pt;
+		curr.doors = 0;
+		curr.steps = 0;
+		queue.emplace(curr);
+		history[pt] = true;
+
+		while (!queue.empty()) {
+			curr = queue.front();
+			queue.pop();
+			history[curr.coords] = true;
+
+			if (curr.coords == room) {
+				results[room].push_back(curr);
+			} else {
+				for (uint32_t i = 0; i < nexts.size(); i++) {
+					tmp = curr;
+					tmp.coords.x += nexts[i].x;
+					tmp.coords.y += nexts[i].y;
+					c = get_map_symbol(tmp.coords);
+					if ((c == '-') || (c == '|')) {
+						tmp.coords.x += nexts[i].x;
+						tmp.coords.y += nexts[i].y;
+						if (!history.count(tmp.coords)) {
+							c = get_map_symbol(tmp.coords);
+							if (c == '.') {
+								tmp.doors++;
+								tmp.steps += 2;
+								queue.emplace(tmp);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	for (auto it = results.begin(); it != results.end(); it++){
+		int32_t steps = INT32_MAX, doors = 0;
+
+		for (uint32_t i = 0; i < it->second.size();++i){
+			if (it ->second[i].steps < steps) {
+				steps = it->second[i].steps;
+				doors = it->second[i].doors;
+			}
+		}
+		if (doors > result) {
+			result = doors;
+		}
+	}
+
+		return result;
 }
 
 int main(void) {
@@ -207,6 +263,8 @@ int main(void) {
 
 	map.print();
 
+	result1 = map.get_furthest_room_passing_doors_count();
+
 	regex = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
 
 	if (!map.decode_map(regex)) {
@@ -215,7 +273,7 @@ int main(void) {
 
 	map.print();
 
-	result1 = 1;
+	result1 = map.get_furthest_room_passing_doors_count();
 
 #endif
 
