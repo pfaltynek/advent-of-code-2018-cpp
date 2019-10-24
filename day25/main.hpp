3dc559bb93516596be54fdfa56289d7cdb549670
Petr Faltýnek <pfaltynek@gmail.com>
2019-10-24 14:18:04 +0200

    day25 - part1 started

---
+++ day25/main.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include <regex>
+#include <map>
+
+#define TEST 0
+
+#endif // MAIN_HPP
