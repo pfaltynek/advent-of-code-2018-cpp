98446f29ed32b3fbd87cf5278861e1404b6f551d
Petr Faltýnek <pfaltynek@gmail.com>
2019-10-25 11:17:03 +0200

    day25- part1 - implentation finished, tests OK, real data no :(

--- day25/main.cpp
+++ day25/main.cpp
@@ -77,6 +77,46 @@ bool Adventure4D::init() {
 	return init(lines);
 }
 
+int32_t Adventure4D::find_constellation() {
+	std::vector<std::vector<point_str>> constellations;
+	std::vector<int32_t> idxs;
+
+	constellations.clear();
+
+	for (uint32_t i = 0; i < points_.size(); i++) {
+		idxs.clear();
+
+		for (uint32_t j = 0; j < constellations.size(); j++) {
+			for (uint32_t k = 0; k < constellations[j].size(); k++) {
+				if (constellations[j][k].get_distance(points_[i]) <= 3) {
+					idxs.push_back(j);
+					break;
+				}
+			}
+		}
+
+		if (idxs.size()) {
+			uint32_t main_idx = idxs[0];
+
+			constellations[main_idx].push_back(points_[i]);
+			idxs.erase(idxs.begin());
+			std::sort(idxs.begin(), idxs.end());
+			for (uint32_t j = 0; j < idxs.size(); j++) {
+				constellations[main_idx].insert(constellations[main_idx].end(), constellations[idxs[j]].begin(), constellations[idxs[j]].end());
+				constellations.erase(constellations.begin() + idxs[j]);
+			}
+		} else {
+			std::vector<point_str> tmp;
+
+			tmp.clear();
+			tmp.push_back(points_[i]);
+			constellations.push_back(tmp);
+		}
+	}
+
+	return constellations.size();
+}
+
 int main(void) {
 	int32_t result1 = 0, result2 = 0;
 	Adventure4D a4d;
@@ -86,7 +126,32 @@ int main(void) {
 		return -1;
 	}
 
-	result1 = 1;
+	result1 = a4d.find_constellation(); // 2
+
+	if (!a4d.init({"0,0,0,0", "3,0,0,0", "0,3,0,0", "0,0,3,0", "0,0,0,3", "0,0,0,6", "9,0,0,0", "12,0,0,0", "6,0,0,0"})) {
+		return -1;
+	}
+
+	result1 = a4d.find_constellation(); // 1
+
+	if (!a4d.init({"-1,2,2,0", "0,0,2,-2", "0,0,0,-2", "-1,2,0,0", "-2,-2,-2,2", "3,0,2,-1", "-1,3,2,2", "-1,0,-1,0", "0,2,1,-2", "3,0,0,0"})) {
+		return -1;
+	}
+
+	result1 = a4d.find_constellation(); // 4
+
+	if (!a4d.init({"1,-1,0,1", "2,0,-1,0", "3,2,-1,0", "0,0,3,1", "0,0,-1,-1", "2,3,-2,0", "-2,2,0,0", "2,-2,0,-1", "1,-1,0,-1", "3,2,0,2"})) {
+		return -1;
+	}
+
+	result1 = a4d.find_constellation(); // 3
+
+	if (!a4d.init({"1,-1,-1,-2", "-2,-2,0,1", "0,2,1,3", "-2,3,-2,1", "0,2,3,-2", "-1,-1,1,-2", "0,-2,-1,0", "-2,2,3,-1", "1,2,2,0", "-1,-2,0,-2"})) {
+		return -1;
+	}
+
+	result1 = a4d.find_constellation(); // 8
+
 	result2 = 2;
 #endif
 
@@ -97,7 +162,7 @@ int main(void) {
 	std::cout << "=== Advent of Code 2018 - day 25 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
-	result1 = 1;
+	result1 = a4d.find_constellation(); //319? too low
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
