f0f982830231bc8e429bd94de9e4612ebe6d0b64
Petr Faltýnek <pfaltynek@gmail.com>
2019-11-05 15:43:34 +0100

    day09 updated to use common aoc solution template

--- day09/main.cpp
+++ day09/main.cpp
@@ -1,10 +1,7 @@
-#include <fstream>
-#include <iostream>
+#include "./../common/aoc.hpp"
 #include <regex>
-#include <vector>
 
-#define TEST1 0
-#define TEST2 0
+#define TEST 0
 
 const std::regex marble_mania_template("^(\\d+) players; last marble is worth (\\d+) points$");
 
@@ -14,36 +11,58 @@ typedef struct marble {
 	marble *next;
 } marble_t, *p_marble_t;
 
-bool DecodeGameConditions(const std::string line, int32_t &players_count, int32_t &last_marble_points) {
+class AoC2018_day09 : public AoC {
+  protected:
+	bool init(const std::vector<std::string> lines);
+	bool part1();
+	bool part2();
+	void tests();
+	int32_t get_aoc_day();
+	int32_t get_aoc_year();
+	void clean_up();
+
+  private:
+	bool decode_game_conditions(const std::string line);
+	void print(p_marble_t current, uint32_t player_idx);
+	void free_list_mem();
+	p_marble_t get_marble_mania_high_score(const int32_t last_marble_points_multiplier, uint32_t &result1, uint32_t &result2);
+
+	int32_t players_count_, last_marble_points_;
+	p_marble_t first_;
+};
+
+bool AoC2018_day09::decode_game_conditions(const std::string line) {
 	std::smatch sm;
 
 	if (std::regex_match(line, sm, marble_mania_template)) {
-		players_count = stoi(sm.str(1));
-		last_marble_points = stoi(sm.str(2));
+		players_count_ = stoi(sm.str(1));
+		last_marble_points_ = stoi(sm.str(2));
 		return true;
 	}
 
 	return false;
 }
 
-void FreeListMem(const p_marble_t first) {
+void AoC2018_day09::free_list_mem() {
 	p_marble_t next, tmp;
 
-	next = first;
+	next = first_;
 	while (next != NULL) {
 		tmp = next;
 		next = tmp->next;
 		delete tmp;
-		if (next == first) {
+		if (next == first_) {
 			next = NULL;
 		}
 	}
+
+	first_ = NULL;
 }
 
-void PrintMarbles(p_marble_t first, p_marble_t current, uint32_t player_idx) {
+void AoC2018_day09::print(p_marble_t current, uint32_t player_idx) {
 	p_marble_t tmp;
 
-	tmp = first;
+	tmp = first_;
 	std::cout << "[" << player_idx << "] ";
 
 	while (tmp != NULL) {
@@ -52,7 +71,7 @@ void PrintMarbles(p_marble_t first, p_marble_t current, uint32_t player_idx) {
 		} else {
 			std::cout << tmp->value;
 		}
-		if (tmp->next == first) {
+		if (tmp->next == first_) {
 			tmp = NULL;
 		} else {
 			std::cout << " ";
@@ -63,16 +82,15 @@ void PrintMarbles(p_marble_t first, p_marble_t current, uint32_t player_idx) {
 	std::cout << std::endl;
 }
 
-p_marble_t GetMarbleManiaHighScore(const int32_t players_count, const int32_t last_marble_points, const int32_t last_marble_points_multiplier,
-								   uint32_t &result1, uint32_t &result2) {
+p_marble_t AoC2018_day09::get_marble_mania_high_score(const int32_t last_marble_points_multiplier, uint32_t &result1, uint32_t &result2) {
 	p_marble_t first = NULL, current = NULL;
 	std::vector<uint32_t> scores;
-	uint32_t player_idx = 0, i, j;
+	int32_t player_idx = 0, i, j;
 
 	result1 = 0;
 	result2 = 0;
 	scores.clear();
-	for (i = 0; i < players_count; ++i) {
+	for (i = 0; i < players_count_; ++i) {
 		scores.push_back(0);
 	}
 
@@ -82,9 +100,9 @@ p_marble_t GetMarbleManiaHighScore(const int32_t players_count, const int32_t la
 	current->next = current;
 	first = current;
 
-	// PrintMarbles(first, current, player_idx);
+	// print(current, player_idx);
 
-	for (i = 1; i <= (last_marble_points * last_marble_points_multiplier); ++i) {
+	for (i = 1; i <= (last_marble_points_ * last_marble_points_multiplier); ++i) {
 		p_marble_t next, prev, tmp;
 
 		if (i % 23) {
@@ -111,20 +129,21 @@ p_marble_t GetMarbleManiaHighScore(const int32_t players_count, const int32_t la
 			current = next;
 		}
 
-		player_idx = (++player_idx) % players_count;
+		player_idx++;
+		player_idx %= players_count_;
 
-		if (i == last_marble_points) {
-			for (j = 0; j < players_count; ++j) {
+		if (i == last_marble_points_) {
+			for (j = 0; j < players_count_; ++j) {
 				if (scores[j] > result1) {
 					result1 = scores[j];
 				}
 			}
 		}
 
-		// PrintMarbles(first, current, player_idx);
+		// print(current, player_idx);
 	}
 
-	for (i = 0; i < players_count; ++i) {
+	for (i = 0; i < players_count_; ++i) {
 		if (scores[i] > result2) {
 			result2 = scores[i];
 		}
@@ -133,79 +152,76 @@ p_marble_t GetMarbleManiaHighScore(const int32_t players_count, const int32_t la
 	return first;
 }
 
-int main(void) {
-	int players_count, last_marble_points;
-	uint32_t result1 = 0, result2 = 0;
-	std::ifstream input;
-	std::string line;
-	p_marble_t first;
-
-	std::cout << "=== Advent of Code 2018 - day 9 ====" << std::endl;
-	std::cout << "--- part 1 ---" << std::endl;
-
-#if TEST1
-	first = GetMarbleManiaHighScore(9, 25, 1, result1, result2); // high score is 32
-	FreeListMem(first);
-
-	line = "10 players; last marble is worth 1618 points"; //: high score is 8317
-	if (DecodeGameConditions(line, players_count, last_marble_points)) {
-		first = GetMarbleManiaHighScore(players_count, last_marble_points, 1, result1, result2);
-		FreeListMem(first);
-	}
-
-	line = "13 players; last marble is worth 7999 points"; //: high score is 146373
-	if (DecodeGameConditions(line, players_count, last_marble_points)) {
-		first = GetMarbleManiaHighScore(players_count, last_marble_points, 1, result1, result2);
-		FreeListMem(first);
-	}
-
-	line = "17 players; last marble is worth 1104 points"; //: high score is 2764
-	if (DecodeGameConditions(line, players_count, last_marble_points)) {
-		first = GetMarbleManiaHighScore(players_count, last_marble_points, 1, result1, result2);
-		FreeListMem(first);
-	}
-
-	line = "21 players; last marble is worth 6111 points"; //: high score is 54718
-	if (DecodeGameConditions(line, players_count, last_marble_points)) {
-		first = GetMarbleManiaHighScore(players_count, last_marble_points, 1, result1, result2);
-		FreeListMem(first);
-	}
-
-	line = "30 players; last marble is worth 5807 points"; //: high score is 37305
-	if (DecodeGameConditions(line, players_count, last_marble_points)) {
-		first = GetMarbleManiaHighScore(players_count, last_marble_points, 1, result1, result2);
-		FreeListMem(first);
-	}
-
-#elif TEST2
-
-#else
-	input.open("input.txt", std::ifstream::in);
-
-	if (input.fail()) {
-		std::cout << "Error opening input file.\n";
-		return -1;
-	}
-
-	while (std::getline(input, line)) {
-		if (!DecodeGameConditions(line, players_count, last_marble_points)) {
-			std::cout << "Invalid game data" << std::endl;
-			return -1;
-		} else {
-			break;
+bool AoC2018_day09::init(std::vector<std::string> lines) {
+	for (uint32_t i = 0; i < lines.size(); i++) {
+		if (!decode_game_conditions(lines[i])) {
+			std::cout << "Invalid instruction at line " << i + 1 << std::endl;
 		}
 	}
 
-	if (input.is_open()) {
-		input.close();
-	}
+	return true;
+}
+
+int32_t AoC2018_day09::get_aoc_day() {
+	return 9;
+}
+
+int32_t AoC2018_day09::get_aoc_year() {
+	return 2018;
+}
+
+void AoC2018_day09::tests() {
+#if TEST
+	init({"9 players; last marble is worth 25 points"}); // high score is 32
+	part1();
+	part2();
+
+	init({"10 players; last marble is worth 1618 points"}); //: high score is 8317
+	part1();
+	part2();
+
+	init({"13 players; last marble is worth 7999 points"}); //: high score is 146373
+	part1();
+	part2();
+
+	init({"17 players; last marble is worth 1104 points"}); //: high score is 2764
+	part1();
+	part2();
+
+	init({"21 players; last marble is worth 6111 points"}); //: high score is 54718
+	part1();
+	part2();
+
+	init({"30 players; last marble is worth 5807 points"}); //: high score is 37305
+	part1();
+	part2();
+
 #endif
+}
 
-	first = GetMarbleManiaHighScore(players_count, last_marble_points, 100, result1, result2);
+bool AoC2018_day09::part1() {
+	uint32_t result1 = 0, result2 = 0;
 
-	std::cout << "Result is " << result1 << std::endl;
-	std::cout << "--- part 2 ---" << std::endl;
-	std::cout << "Result is " << result2 << std::endl;
+	first_ = get_marble_mania_high_score(100, result1, result2);
+
+	result1_ = std::to_string(result1);
+	result2_ = std::to_string(result2);
+
+	return true;
+}
+
+bool AoC2018_day09::part2() {
+
+	return true;
+}
+
+void AoC2018_day09::clean_up() {
 	std::cout << std::endl << "Please wait, clean-up in progress ..." <<std::endl;
-	FreeListMem(first);
+	free_list_mem();
+}
+
+int main(void) {
+	AoC2018_day09 day09;
+
+	return day09.main_execution();
 }
