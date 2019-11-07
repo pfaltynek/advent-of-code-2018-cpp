1404320e1a42296c01944d684d13c62877c51345
Petr Faltýnek <pfaltynek@gmail.com>
2019-11-07 08:04:06 +0100

    day13 updated to use common aoc solution template

--- day13/main.cpp
+++ day13/main.cpp
@@ -1,50 +1,112 @@
-#include <fstream>
-#include <iostream>
+#include "./../common/aoc.hpp"
+#include "./../common/coord.hpp"
 #include <map>
-#include <vector>
 
-#define TEST1 0
-#define TEST2 0 
+#define TEST 1
 
 typedef enum CART_HEAD { HEAD_UP = '^', HEAD_DOWN = 'v', HEAD_LEFT = '<', HEAD_RIGHT = '>' } cart_head_t;
 typedef enum CART_TURN { TURN_LEFT = 0, TURN_STRAIGHT = 1, TURN_RIGHT = 2 } cart_turn_t;
 
 typedef struct cart {
-	uint32_t x;
-	uint32_t y;
+	coord_str coord;
 	cart_head_t head;
 	cart_turn_t next_turn;
 	bool removed;
+	uint64_t get_coord() {
+		uint64_t result = coord.x;
+		result += coord.y * 1000;
+		return result;
+	}
 } cart_str;
 
-uint64_t GetCartCoord(const cart_str cart) {
-	uint64_t result;
+class AoC2018_day13 : public AoC {
+  protected:
+	bool init(const std::vector<std::string> lines);
+	bool part1();
+	bool part2();
+	void tests();
+	int32_t get_aoc_day();
+	int32_t get_aoc_year();
 
-	result = cart.x;
-	result += cart.y * 1000;
+  private:
+	bool simulate_cart_move(cart_str &cart);
+	bool simulate_carts_movement_till_first_crash(coord_str &crash);
+	bool simulate_carts_movement_till_last_remaining(coord_str &last);
+	std::vector<cart_str> carts_;
+	std::vector<std::string> map_;
+};
 
-	return result;
+bool AoC2018_day13::init(std::vector<std::string> lines) {
+	uint32_t map_width;
+	std::string cart_head;
+
+	cart_head.clear();
+	cart_head += (char)HEAD_UP;
+	cart_head += (char)HEAD_DOWN;
+	cart_head += (char)HEAD_LEFT;
+	cart_head += (char)HEAD_RIGHT;
+
+	carts_.clear();
+
+	for (uint32_t i = 0; i < lines.size(); ++i) {
+		size_t pos = 0;
+
+		if (i) {
+			if (lines[i].size() != map_width) {
+				std::cout << "Invalid input size at line " << i + 1 << std::endl;
+				return false;
+			}
+		} else {
+			map_width = lines[i].size();
+		}
+
+		pos = lines[i].find_first_of(cart_head, pos);
+
+		while (pos != std::string::npos) {
+			cart_str cart = {};
+			cart.coord.x = pos;
+			cart.coord.y = i;
+			cart.head = (cart_head_t)lines[i][pos];
+			cart.next_turn = CART_TURN::TURN_LEFT;
+			cart.removed = false;
+			carts_.push_back(cart);
+
+			if ((lines[i][pos] == HEAD_DOWN) || (lines[i][pos] == HEAD_UP)) {
+				lines[i][pos] = '|';
+			}
+
+			if ((lines[i][pos] == HEAD_LEFT) || (lines[i][pos] == HEAD_RIGHT)) {
+				lines[i][pos] = '-';
+			}
+
+			pos = lines[i].find_first_of(cart_head, pos);
+		}
+	}
+
+	map_ = lines;
+
+	return true;
 }
 
-bool SimulateCartMove(const std::vector<std::string> &map, cart_str &cart) {
+bool AoC2018_day13::simulate_cart_move(cart_str &cart) {
 	switch (cart.head) {
 		case CART_HEAD::HEAD_UP:
-			cart.y--;
+			cart.coord.y--;
 			break;
 		case CART_HEAD::HEAD_DOWN:
-			cart.y++;
+			cart.coord.y++;
 			break;
 		case CART_HEAD::HEAD_LEFT:
-			cart.x--;
+			cart.coord.x--;
 			break;
 		case CART_HEAD::HEAD_RIGHT:
-			cart.x++;
+			cart.coord.x++;
 			break;
 		default:
 			return false;
 	}
 
-	switch (map[cart.y][cart.x]) {
+	switch (map_[cart.coord.y][cart.coord.x]) {
 		case '-':
 			if ((cart.head == CART_HEAD::HEAD_UP) || (cart.head == CART_HEAD::HEAD_DOWN)) {
 				return false;
@@ -138,39 +200,31 @@ bool SimulateCartMove(const std::vector<std::string> &map, cart_str &cart) {
 	return true;
 }
 
-bool SimulateCartsMovementTillFirstCrash(const std::vector<std::string> &map, std::vector<cart_str> carts, uint32_t &crash_x, uint32_t &crash_y) {
+bool AoC2018_day13::simulate_carts_movement_till_first_crash(coord_str &crash) {
 	std::map<uint64_t, uint32_t> cart_order, tmp;
 	uint32_t i;
-	uint64_t coord;
-	uint32_t cnt;
-
-	cnt = 0;
+	std::vector<cart_str> carts = carts_;
 
 	while (true) {
 		cart_order.clear();
 		tmp.clear();
 
-		cnt++;
-
 		for (i = 0; i < carts.size(); ++i) {
-			coord = GetCartCoord(carts[i]);
-			cart_order[coord] = i;
+			cart_order[carts[i].get_coord()] = i;
 		}
 
 		for (auto it = cart_order.begin(); it != cart_order.end(); ++it) {
 			i = it->second;
 
-			if (!SimulateCartMove(map, carts[i])) {
+			if (!simulate_cart_move(carts[i])) {
 				return false;
 			}
 
-			coord = GetCartCoord(carts[i]);
-			if (tmp.count(coord)) {
-				crash_x = carts[i].x;
-				crash_y = carts[i].y;
+			if (tmp.count(carts[i].get_coord())) {
+				crash = carts[i].coord;
 				return true;
 			} else {
-				tmp[coord] = i;
+				tmp[carts[i].get_coord()] = i;
 			}
 		}
 	}
@@ -178,36 +232,31 @@ bool SimulateCartsMovementTillFirstCrash(const std::vector<std::string> &map, st
 	return false;
 }
 
-bool SimulateCartsMovementTillLastRemaining(const std::vector<std::string> &map, std::vector<cart_str> carts, uint32_t &last_x, uint32_t &last_y) {
+bool AoC2018_day13::simulate_carts_movement_till_last_remaining(coord_str &last) {
 	std::map<uint64_t, uint32_t> cart_order, tmp;
-	uint32_t i, j, x1, x2;
-	uint64_t coord;
-	uint32_t cnt;
-	cnt = 0;
+	uint32_t i, j;
+	std::vector<cart_str> carts = carts_;
 
 	while (carts.size() >= 2) {
 		cart_order.clear();
 		tmp.clear();
 
-		cnt++;
-
 		for (i = 0; i < carts.size(); ++i) {
-			coord = GetCartCoord(carts[i]);
-			cart_order[coord] = i;
+			cart_order[carts[i].get_coord()] = i;
 		}
 
 		for (auto it = cart_order.begin(); it != cart_order.end(); ++it) {
 			i = it->second;
 
 			if (!carts[i].removed) {
-				if (!SimulateCartMove(map, carts[i])) {
+				if (!simulate_cart_move(carts[i])) {
 					return false;
 				}
 			}
 
 			for (j = 0; j < carts.size(); ++j) {
 				if (j != i) {
-					if ((carts[i].x == carts[j].x) && (carts[i].y == carts[j].y)) {
+					if ((carts[i].coord.x == carts[j].coord.x) && (carts[i].coord.y == carts[j].coord.y)) {
 						carts[i].removed = true;
 						carts[j].removed = true;
 						break;
@@ -227,144 +276,73 @@ bool SimulateCartsMovementTillLastRemaining(const std::vector<std::string> &map,
 	}
 
 	if (carts.size()) {
-		last_x = carts[0].x;
-		last_y = carts[0].y;
+		last = carts[0].coord;
 		return true;
 	}
 
 	return false;
 }
 
-bool DecodeMap(std::vector<std::string> &map, std::vector<cart_str> &carts) {
-	uint32_t map_width;
-	std::string cart_head;
+int32_t AoC2018_day13::get_aoc_day() {
+	return 13;
+}
 
-	cart_head.clear();
-	cart_head += (char)HEAD_UP;
-	cart_head += (char)HEAD_DOWN;
-	cart_head += (char)HEAD_LEFT;
-	cart_head += (char)HEAD_RIGHT;
+int32_t AoC2018_day13::get_aoc_year() {
+	return 2018;
+}
 
-	for (uint32_t i = 0; i < map.size(); ++i) {
-		size_t pos = 0;
+void AoC2018_day13::tests() {
+#if TEST
+	/*
+/->-\
+|   |  /----\
+| /-+--+-\  |
+| | |  | v  |
+\-+-/  \-+--/
+  \------/
+*/
+	init({"/->-\\        ", "|   |  /----\\", "| /-+--+-\\  |", "| | |  | v  |", "\\-+-/  \\-+--/", "  \\------/   "});
+	part1();
 
-		if (i) {
-			if (map[i].size() != map_width) {
-				std::cout << "Invalid input size at line " << i + 1 << std::endl;
-				return false;
-			}
-		} else {
-			map_width = map[i].size();
-		}
+	/*
+/>-<\
+|   |
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/
+*/
+	init({"/>-<\\  ", "|   |  ", "| /<+-\\", "| | | v", "\\>+</ |", "  |   ^", "  \\<->/"});
+	part2();
 
-		pos = map[i].find_first_of(cart_head, pos);
+#endif
+}
 
-		while (pos != std::string::npos) {
-			cart_str cart = {};
-			cart.x = pos;
-			cart.y = i;
-			cart.head = (cart_head_t)map[i][pos];
-			cart.next_turn = CART_TURN::TURN_LEFT;
-			cart.removed = false;
-			carts.push_back(cart);
+bool AoC2018_day13::part1() {
+	coord_str result;
 
-			if ((map[i][pos] == HEAD_DOWN) || (map[i][pos] == HEAD_UP)) {
-				map[i][pos] = '|';
-			}
-
-			if ((map[i][pos] == HEAD_LEFT) || (map[i][pos] == HEAD_RIGHT)) {
-				map[i][pos] = '-';
-			}
-
-			pos = map[i].find_first_of(cart_head, pos);
-		}
+	if (simulate_carts_movement_till_first_crash(result)) {
+		result1_ = std::to_string(result.x) + "," + std::to_string(result.y);
+		return true;
 	}
 
-	return true;
+	return false;
+}
+
+bool AoC2018_day13::part2() {
+	coord_str result;
+
+	if (simulate_carts_movement_till_last_remaining(result)) {
+		result2_ = std::to_string(result.x) + "," + std::to_string(result.y);
+		return true;
+	}
+
+	return false;
 }
 
 int main(void) {
-	int cnt = 0;
-	uint32_t result1x = 0, result1y = 0, result2x = 0, result2y = 0;
-	std::ifstream input;
-	std::string line;
-	std::vector<std::string> map;
-	std::vector<cart_str> carts;
+	AoC2018_day13 day05;
 
-	std::cout << "=== Advent of Code 2018 - day 13 ====" << std::endl;
-	std::cout << "--- part 1 ---" << std::endl;
-
-	map.clear();
-	carts.clear();
-
-#if TEST1
-	/*
-	/->-\
-	|   |  /----\
-	| /-+--+-\  |
-	| | |  | v  |
-	\-+-/  \-+--/
-	  \------/
-	*/
-	map.push_back("/->-\\        ");
-	map.push_back("|   |  /----\\");
-	map.push_back("| /-+--+-\\  |");
-	map.push_back("| | |  | v  |");
-	map.push_back("\\-+-/  \\-+--/");
-	map.push_back("  \\------/   ");
-#elif TEST2
-	/*
-	/>-<\
-	|   |
-	| /<+-\
-	| | | v
-	\>+</ |
-	  |   ^
-	  \<->/
-	*/
-	map.push_back("/>-<\\  ");
-	map.push_back("|   |  ");
-	map.push_back("| /<+-\\");
-	map.push_back("| | | v");
-	map.push_back("\\>+</ |");
-	map.push_back("  |   ^");
-	map.push_back("  \\<->/");
-#else
-	input.open("input.txt", std::ifstream::in);
-
-	if (input.fail()) {
-		std::cout << "Error opening input file.\n";
-		return -1;
-	}
-
-	while (std::getline(input, line)) {
-
-		map.push_back(line);
-		cnt++;
-	}
-
-	if (input.is_open()) {
-		input.close();
-	}
-
-#endif
-
-	if (!DecodeMap(map, carts)) {
-		return -1;
-	}
-
-	if (!SimulateCartsMovementTillFirstCrash(map, carts, result1x, result1y)) {
-		std::cout << "Simulation failed" << std::endl;
-		return -1;
-	}
-
-	std::cout << "Result is " << result1x << "," << result1y << std::endl;
-
-	if (!SimulateCartsMovementTillLastRemaining(map, carts, result2x, result2y)) {
-		std::cout << "Simulation failed" << std::endl;
-		return -1;
-	}
-
-	std::cout << "--- part 2 ---" << std::endl;
-	std::cout << "Result is " << result2x << "," << result2y << std::endl;
+	return day05.main_execution();
 }
