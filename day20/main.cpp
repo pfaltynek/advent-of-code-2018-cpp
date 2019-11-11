7ab729d3c9b55808426a99949d6a416a2627f9d7
Petr Faltýnek <pfaltynek@gmail.com>
2019-11-11 12:25:41 +0100

    day20 updated to use common aoc solution template

--- day20/main.cpp
+++ day20/main.cpp
@@ -1,4 +1,11 @@
-#include "main.hpp"
+#include "./../common/aoc.hpp"
+#include "./../common/coord.hpp"
+#include <map>
+#include <queue>
+#include <stack>
+
+#define TEST 1
+#define DEBUG_PRINT 0
 
 typedef struct STEP_INFO {
 	coord_str coords;
@@ -6,47 +13,39 @@ typedef struct STEP_INFO {
 	int32_t doors;
 } step_info_str;
 
-class RoomsMap {
-  public:
-	bool init(std::string& regex);
+class AoC2018_day20 : public AoC {
+  protected:
+	bool init(const std::vector<std::string> lines);
+	bool part1();
+	bool part2();
+	void tests();
+	int32_t get_aoc_day();
+	int32_t get_aoc_year();
+
+  private:
 	void print();
 	int32_t get_furthest_room_passing_doors_count(int32_t& count1000);
 	bool decode_map(const std::string regex);
 	char get_map_symbol(const coord_str coord);
 	char get_map_symbol(int32_t x, int32_t y);
 
-  private:
 	std::map<coord_str, char> map_;
 	std::map<coord_str, std::vector<coord_str>> doors_map_;
 	coord_str min_, doors_min_;
 	coord_str max_, doors_max_;
 };
 
-bool RoomsMap::init(std::string& regex) {
-	std::ifstream input;
-	std::string line;
+bool AoC2018_day20::init(const std::vector<std::string> lines) {
+	std::string regex;
 
-	input.open("input.txt", std::ifstream::in);
-
-	if (input.fail()) {
-		std::cout << "Error opening input file.\n";
-		return false;
+	for (uint32_t i = 0; i < lines.size(); ++i) {
+		regex.append(lines[i]);
 	}
 
-	regex.clear();
-
-	while (std::getline(input, line)) {
-		regex.append(line);
-	}
-
-	if (input.is_open()) {
-		input.close();
-	}
-
-	return true;
+	return decode_map(regex);
 }
 
-bool RoomsMap::decode_map(const std::string regex) {
+bool AoC2018_day20::decode_map(const std::string regex) {
 	coord_str coord, new_coord, doors_coord, new_doors_coord;
 	std::stack<coord_str> stack, doors_stack;
 
@@ -184,7 +183,7 @@ bool RoomsMap::decode_map(const std::string regex) {
 	return true;
 }
 
-char RoomsMap::get_map_symbol(const coord_str coord) {
+char AoC2018_day20::get_map_symbol(const coord_str coord) {
 	if (map_.count(coord)) {
 		return map_[coord];
 	} else {
@@ -192,11 +191,12 @@ char RoomsMap::get_map_symbol(const coord_str coord) {
 	}
 }
 
-char RoomsMap::get_map_symbol(int32_t x, int32_t y) {
+char AoC2018_day20::get_map_symbol(int32_t x, int32_t y) {
 	return get_map_symbol(coord_str(x, y));
 }
 
-void RoomsMap::print() {
+void AoC2018_day20::print() {
+#if DEBUG_PRINT
 	for (int32_t i = max_.y + 1; i >= min_.y - 1; --i) {
 		for (int32_t j = min_.x - 1; j <= max_.x + 1; ++j) {
 			coord_str coord = {j, i};
@@ -209,9 +209,10 @@ void RoomsMap::print() {
 		std::cout << std::endl;
 	}
 	std::cout << std::endl;
+#endif
 }
 
-int32_t RoomsMap::get_furthest_room_passing_doors_count(int32_t& count1000) {
+int32_t AoC2018_day20::get_furthest_room_passing_doors_count(int32_t& count1000) {
 	step_info_str curr, tmp;
 	std::map<coord_str, int32_t> history;
 	std::queue<step_info_str> queue;
@@ -254,42 +255,32 @@ int32_t RoomsMap::get_furthest_room_passing_doors_count(int32_t& count1000) {
 	return result;
 }
 
-int main(void) {
-	int32_t result1 = 0, result2 = 0;
-	std::string regex;
-	RoomsMap map;
+int32_t AoC2018_day20::get_aoc_day() {
+	return 20;
+}
+
+int32_t AoC2018_day20::get_aoc_year() {
+	return 2018;
+}
+
+void AoC2018_day20::tests() {
 #if TEST
+	int32_t result1, result2;
 
-	regex = "^ENWWW(NEEE|SSE(EE|N))$";
-
-	if (!map.decode_map(regex)) {
-		return -1;
+	if (init({"^ENWWW(NEEE|SSE(EE|N))$"})) {
+		print();
+		result1 = get_furthest_room_passing_doors_count(result2);
 	}
 
-	map.print();
-
-	result1 = map.get_furthest_room_passing_doors_count(result2);
-
-	regex = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
-
-	if (!map.decode_map(regex)) {
-		return -1;
+	if (init({"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"})) {
+		print();
+		result1 = get_furthest_room_passing_doors_count(result2);
 	}
 
-	map.print();
-
-	result1 = map.get_furthest_room_passing_doors_count(result2);
-
-	regex = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
-
-	if (!map.decode_map(regex)) {
-		return -1;
+	if (init({"^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"})) {
+		print();
+		result1 = get_furthest_room_passing_doors_count(result2);
 	}
-
-	map.print();
-
-	result1 = map.get_furthest_room_passing_doors_count(result2);
-
 	// Furthest room requires passing 23 doors
 	/*
 	#############
@@ -307,54 +298,49 @@ int main(void) {
 	#############
 	*/
 
-	regex = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
-
-	if (!map.decode_map(regex)) {
-		return -1;
+	if (init({"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"})) {
+		print();
+		result1 = get_furthest_room_passing_doors_count(result2);
 	}
-
-	map.print();
-
-	result1 = map.get_furthest_room_passing_doors_count(result2);
-
-// Furthest room requires passing 31 doors
-/*
-###############
-#.|.|.|.#.|.|.#
-#-###-###-#-#-#
-#.|.#.|.|.#.#.#
-#-#########-#-#
-#.#.|.|.|.|.#.#
-#-#-#########-#
-#.#.#.|X#.|.#.#
-###-#-###-#-#-#
-#.|.#.#.|.#.|.#
-#-###-#####-###
-#.|.#.|.|.#.#.#
-#-#-#####-#-#-#
-#.#.|.|.|.#.|.#
-###############
-*/
+		// Furthest room requires passing 31 doors
+		/*
+		###############
+		#.|.|.|.#.|.|.#
+		#-###-###-#-#-#
+		#.|.#.|.|.#.#.#
+		#-#########-#-#
+		#.#.|.|.|.|.#.#
+		#-#-#########-#
+		#.#.#.|X#.|.#.#
+		###-#-###-#-#-#
+		#.|.#.#.|.#.|.#
+		#-###-#####-###
+		#.|.#.|.|.#.#.#
+		#-#-#####-#-#-#
+		#.#.|.|.|.#.|.#
+		###############
+		*/
 
 #endif
+}
 
-	if (!map.init(regex)) {
-		return -1;
-	}
+bool AoC2018_day20::part1() {
+	int32_t result1, result2;
 
-	if (!map.decode_map(regex)) {
-		return -1;
-	}
+	result1 = get_furthest_room_passing_doors_count(result2);
 
-	// map.print();
+	result1_ = std::to_string(result1);
+	result2_ = std::to_string(result2);
 
-	std::cout << "=== Advent of Code 2018 - day 20 ====" << std::endl;
-	std::cout << "--- part 1 ---" << std::endl;
+	return true;
+}
 
-	result1 = map.get_furthest_room_passing_doors_count(result2);
+bool AoC2018_day20::part2() {
+	return true;
+}
 
-	std::cout << "Result is " << result1 << std::endl;
-	std::cout << "--- part 2 ---" << std::endl;
+int main(void) {
+	AoC2018_day20 day20;
 
-	std::cout << "Result is " << result2 << std::endl;
+	return day20.main_execution();
 }
