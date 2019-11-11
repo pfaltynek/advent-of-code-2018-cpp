97279f84f7ced589dd38c5890abfaa7890d13936
Petr Faltýnek <pfaltynek@gmail.com>
2019-11-11 11:15:17 +0100

    day19 updated to use common aoc solution template

--- day19/main.cpp
+++ day19/main.cpp
@@ -1,4 +1,8 @@
-#include "main.hpp"
+#include "./../common/aoc.hpp"
+#include <regex>
+
+#define TEST 1
+#define DEBUG_PRINT 0
 
 const std::regex instr1("^#ip (\\d)$");
 const std::regex instr2("^(addr|addi|mulr|muli|banr|bani|borr|bori|setr|seti|gtir|gtri|gtrr|eqir|eqri|eqrr) (\\d) (\\d+) (\\d)$");
@@ -34,7 +38,24 @@ typedef struct INSTRUCTION {
 	int32_t A, B, C;
 } instruction_str;
 
-bool init(const std::vector<std::string> input, std::vector<instruction_str>& data, int32_t& ip_reg_idx) {
+class AoC2018_day19 : public AoC {
+  protected:
+	bool init(const std::vector<std::string> lines);
+	bool part1();
+	bool part2();
+	void tests();
+	int32_t get_aoc_day();
+	int32_t get_aoc_year();
+
+  private:
+	int32_t simulate(const int32_t reg0_init);
+	int32_t simulation_after_reversing(const int32_t reg0_init);
+
+	int32_t ip_reg_idx_;
+	std::vector<instruction_str> data_;
+};
+
+bool AoC2018_day19::init(const std::vector<std::string> lines) {
 	std::smatch sm;
 	std::map<std::string, instruction_t> instruction_types = {
 		{"addr", INSTRUCTION_TYPE::addr}, {"addi", INSTRUCTION_TYPE::addi}, {"mulr", INSTRUCTION_TYPE::mulr}, {"muli", INSTRUCTION_TYPE::muli},
@@ -42,25 +63,25 @@ bool init(const std::vector<std::string> input, std::vector<instruction_str>& da
 		{"setr", INSTRUCTION_TYPE::setr}, {"seti", INSTRUCTION_TYPE::seti}, {"gtir", INSTRUCTION_TYPE::gtir}, {"gtri", INSTRUCTION_TYPE::gtri},
 		{"gtrr", INSTRUCTION_TYPE::gtrr}, {"eqir", INSTRUCTION_TYPE::eqir}, {"eqri", INSTRUCTION_TYPE::eqri}, {"eqrr", INSTRUCTION_TYPE::eqrr}};
 
-	data.clear();
+	data_.clear();
 
-	if (!input.size()) {
+	if (!lines.size()) {
 		std::cout << "Empty input data" << std::endl;
 		return false;
 	}
 
-	if (std::regex_match(input[0], sm, instr1)) {
+	if (std::regex_match(lines[0], sm, instr1)) {
 		std::string x = sm.str(1);
-		ip_reg_idx = stoi(sm.str(1));
+		ip_reg_idx_ = stoi(sm.str(1));
 	} else {
 		std::cout << "Invalid input - missing #ip declaration" << std::endl;
 		return false;
 	}
 
-	for (uint32_t i = 1; i < input.size(); ++i) {
+	for (uint32_t i = 1; i < lines.size(); ++i) {
 		instruction_str inst;
 
-		if (std::regex_match(input[i], sm, instr2)) {
+		if (std::regex_match(lines[i], sm, instr2)) {
 			std::string inst_name = sm.str(1);
 			if (instruction_types.count(inst_name)) {
 				inst.opcode = instruction_types[inst_name];
@@ -72,7 +93,7 @@ bool init(const std::vector<std::string> input, std::vector<instruction_str>& da
 				return false;
 			}
 
-			data.push_back(inst);
+			data_.push_back(inst);
 		} else {
 			std::cout << "Invalid instruction format at line " << i + 1 << std::endl;
 			return false;
@@ -82,124 +103,131 @@ bool init(const std::vector<std::string> input, std::vector<instruction_str>& da
 	return true;
 }
 
-bool init(std::vector<instruction_str>& data, int32_t& ip_reg_idx) {
-	std::ifstream input;
-	std::string line;
-	std::vector<std::string> lines;
-
-	input.open("input.txt", std::ifstream::in);
-
-	if (input.fail()) {
-		std::cout << "Error opening input file.\n";
-		return false;
-	}
-
-	lines.clear();
-
-	while (std::getline(input, line)) {
-		lines.push_back(line);
-	}
-
-	if (input.is_open()) {
-		input.close();
-	}
-
-	return init(lines, data, ip_reg_idx);
-}
-
-int32_t simulate(const std::vector<instruction_str> program, const int32_t ip_reg_idx, const int32_t reg0_init) {
+int32_t AoC2018_day19::simulate(const int32_t reg0_init) {
 	int32_t regs[6] = {0, 0, 0, 0, 0, 0};
-	int32_t ip = 0;
+	uint32_t ip = 0;
 
 	regs[0] = reg0_init;
 
-	while ((ip >= 0) && (ip < program.size())) {
-		regs[ip_reg_idx] = ip;
+	while ((ip >= 0) && (ip < data_.size())) {
+		regs[ip_reg_idx_] = ip;
 
-		switch (program[ip].opcode) {
+		switch (data_[ip].opcode) {
 			case INSTRUCTION_TYPE::addr:
-				regs[program[ip].C] = regs[program[ip].A] + regs[program[ip].B];
+				regs[data_[ip].C] = regs[data_[ip].A] + regs[data_[ip].B];
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " + R" << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " + R" << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::addi:
-				regs[program[ip].C] = regs[program[ip].A] + program[ip].B;
+				regs[data_[ip].C] = regs[data_[ip].A] + data_[ip].B;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " + " << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " + " << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::mulr:
-				regs[program[ip].C] = regs[program[ip].A] * regs[program[ip].B];
+				regs[data_[ip].C] = regs[data_[ip].A] * regs[data_[ip].B];
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " * R" << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " * R" << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::muli:
-				regs[program[ip].C] = regs[program[ip].A] * program[ip].B;
+				regs[data_[ip].C] = regs[data_[ip].A] * data_[ip].B;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " * " << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " * " << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::banr:
-				regs[program[ip].C] = regs[program[ip].A] & regs[program[ip].B];
+				regs[data_[ip].C] = regs[data_[ip].A] & regs[data_[ip].B];
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " & R" << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " & R" << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::bani:
-				regs[program[ip].C] = regs[program[ip].A] & program[ip].B;
+				regs[data_[ip].C] = regs[data_[ip].A] & data_[ip].B;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " & " << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " & " << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::borr:
-				regs[program[ip].C] = regs[program[ip].A] | regs[program[ip].B];
+				regs[data_[ip].C] = regs[data_[ip].A] | regs[data_[ip].B];
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " | R" << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " | R" << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::bori:
-				regs[program[ip].C] = regs[program[ip].A] | program[ip].B;
+				regs[data_[ip].C] = regs[data_[ip].A] | data_[ip].B;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << " | R" << program[ip].B << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << " | R" << data_[ip].B << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::setr:
-				regs[program[ip].C] = regs[program[ip].A];
+				regs[data_[ip].C] = regs[data_[ip].A];
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = R" << program[ip].A << std::endl;
+						  << "R" << data_[ip].C << " = R" << data_[ip].A << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::seti:
-				regs[program[ip].C] = program[ip].A;
+				regs[data_[ip].C] = data_[ip].A;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = " << program[ip].A << std::endl;
+						  << "R" << data_[ip].C << " = " << data_[ip].A << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::gtir:
-				regs[program[ip].C] = program[ip].A > regs[program[ip].B] ? 1 : 0;
+				regs[data_[ip].C] = data_[ip].A > regs[data_[ip].B] ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (" << program[ip].A << " > R" << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (" << data_[ip].A << " > R" << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::gtri:
-				regs[program[ip].C] = regs[program[ip].A] > program[ip].B ? 1 : 0;
+				regs[data_[ip].C] = regs[data_[ip].A] > data_[ip].B ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (R" << program[ip].A << " > " << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (R" << data_[ip].A << " > " << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::gtrr:
-				regs[program[ip].C] = regs[program[ip].A] > regs[program[ip].B] ? 1 : 0;
+				regs[data_[ip].C] = regs[data_[ip].A] > regs[data_[ip].B] ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (R" << program[ip].A << " > R" << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (R" << data_[ip].A << " > R" << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::eqir:
-				regs[program[ip].C] = program[ip].A == regs[program[ip].B] ? 1 : 0;
+				regs[data_[ip].C] = data_[ip].A == regs[data_[ip].B] ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (" << program[ip].A << " == R" << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (" << data_[ip].A << " == R" << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::eqri:
-				regs[program[ip].C] = regs[program[ip].A] == program[ip].B ? 1 : 0;
+				regs[data_[ip].C] = regs[data_[ip].A] == data_[ip].B ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (R" << program[ip].A << " == " << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (R" << data_[ip].A << " == " << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 			case INSTRUCTION_TYPE::eqrr:
-				regs[program[ip].C] = regs[program[ip].A] == regs[program[ip].B] ? 1 : 0;
+				regs[data_[ip].C] = regs[data_[ip].A] == regs[data_[ip].B] ? 1 : 0;
+#if DEBUG_PRINT
 				std::cout << ip << ": "
-						  << "R" << program[ip].C << " = (R" << program[ip].A << " == R" << program[ip].B << ")" << std::endl;
+						  << "R" << data_[ip].C << " = (R" << data_[ip].A << " == R" << data_[ip].B << ")" << std::endl;
+#endif
 				break;
 		}
 
-		ip = regs[ip_reg_idx];
+		ip = regs[ip_reg_idx_];
 		ip++;
 	}
 
@@ -208,8 +236,8 @@ int32_t simulate(const std::vector<instruction_str> program, const int32_t ip_re
 
 // following function is result of reverse engineering of given input
 // function searches sum of all common divisors for numbers 961 and 10551361
-int32_t simulation_after_reversing(const std::vector<instruction_str> program, const int32_t ip_reg_idx, const int32_t reg0_init) {
-	int32_t r0, r4, r5, r6;
+int32_t AoC2018_day19::simulation_after_reversing(const int32_t reg0_init) {
+	int32_t r0, r4;
 
 	r4 = 961;
 	r0 = 0;
@@ -218,7 +246,7 @@ int32_t simulation_after_reversing(const std::vector<instruction_str> program, c
 		r4 += 10550400;
 	}
 
-	for (uint32_t r1 = 1; r1 <= r4; r1++) {
+	for (int32_t r1 = 1; r1 <= r4; r1++) {
 		if (!(r4 % r1)) {
 			r0 += r1;
 		}
@@ -227,35 +255,49 @@ int32_t simulation_after_reversing(const std::vector<instruction_str> program, c
 	return r0;
 }
 
-int main(void) {
-	uint64_t result1 = 0, result2 = 0;
-	int32_t ip;
-	std::vector<instruction_str> data;
+int32_t AoC2018_day19::get_aoc_day() {
+	return 19;
+}
 
+int32_t AoC2018_day19::get_aoc_year() {
+	return 2018;
+}
+
+void AoC2018_day19::tests() {
 #if TEST
-	if (!init({"#ip 0", "seti 5 0 1", "seti 6 0 2", "addi 0 1 0", "addr 1 2 3", "setr 1 0 0", "seti 8 0 4", "seti 9 0 5"}, data, ip)) {
-		return -1;
-	}
+	uint64_t result1;
 
-	result1 = simulation_after_reversing(data, ip, 0);
+	if (init({"#ip 0", "seti 5 0 1", "seti 6 0 2", "addi 0 1 0", "addr 1 2 3", "setr 1 0 0", "seti 8 0 4", "seti 9 0 5"})) {
+		result1 = simulate(0);
+	}
 
 #endif
+}
 
-	if (!init(data, ip)) {
-		return -1;
-	}
+bool AoC2018_day19::part1() {
+	uint64_t result1;
 
-	std::cout << "=== Advent of Code 2018 - day 19 ====" << std::endl;
-	std::cout << "--- part 1 ---" << std::endl;
+	// result1 = simulate(0);
+	result1 = simulation_after_reversing(0);
 
-	// result1 = simulate(data, ip, 0);
-	result1 = simulation_after_reversing(data, ip, 0);
+	result1_ = std::to_string(result1);
 
-	std::cout << "Result is " << result1 << std::endl;
-	std::cout << "--- part 2 ---" << std::endl;
+	return true;
+}
 
-	// result2 = simulate(data, ip, 1);
-	result2 = simulation_after_reversing(data, ip, 1);
+bool AoC2018_day19::part2() {
+	uint64_t result2;
 
-	std::cout << "Result is " << result2 << std::endl;
+	// result2 = simulate(1);
+	result2 = simulation_after_reversing(1);
+
+	result2_ = std::to_string(result2);
+
+	return true;
+}
+
+int main(void) {
+	AoC2018_day19 day19;
+
+	return day19.main_execution();
 }
